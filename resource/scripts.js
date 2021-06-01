0c028c403de25dac0e3aba3477607a5dc93d26c3
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-06-01 13:46:17 +0800

    Update scripts.js

--- resource/scripts.js
+++ resource/scripts.js
@@ -1,1 +1,13 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("togglebtn").innerHTML = "X";
+  document.getElementById("openbtn").onclick = "closeNav()";
+}
 
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("togglebtn").innerHTML = "☰";
+  document.getElementById("togglebtn").onclick = "openNav()";
+}
