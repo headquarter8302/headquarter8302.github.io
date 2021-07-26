e6362aa087b91febf176e280e53cbd5f233ac741
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-07-26 13:57:53 +0800

    Update script.js

--- resource/script.js
+++ resource/script.js
@@ -1,3 +1,9 @@
 // Prevent cookies to being set, at all.
 document.__defineGetter__("cookie", function() { return '';} );
 document.__defineSetter__("cookie", function() {} );
+
+
+console.log(
+        "%cHello! :)",
+        "color:red;font-family:monospace;font-size:3rem;font-weight:bold"
+      );
