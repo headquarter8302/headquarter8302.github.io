d4f52c402ea9f24c4601585e3c1c4f3c771c91c6
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-10-21 18:35:37 +0800

    didn't work

--- resource/search.js
+++ resource/search.js
@@ -6,21 +6,17 @@ const searchBing = document.getElementById("bing");
 
 const searchWP = document.getElementById("wikipedia");
 
-function update() {
-  var searchInput = document.getElementById("searchbox").value;
-}
-
 searchGoogle.onclick = function() {
-  update()
+    var searchInput = document.getElementById("searchbox").value;
   window.location.href("https://google.com/search?q=" + searchInput);
 };
 
 searchBing.onclick = function() {
-  update()
+    var searchInput = document.getElementById("searchbox").value;
   window.location.href("https://bing.com/search?q=" + searchInput);
 };
 
 searchWP.onclick = function() {
-  update()
+    var searchInput = document.getElementById("searchbox").value;
   window.location.href("https://en.wikipedia.org/w/index.php?title=Special:Search&search=" + searchInput);
 }
