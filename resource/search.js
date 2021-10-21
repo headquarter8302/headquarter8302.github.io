a8b1f478e63e3868bcb87a9301577bb2c9daf45c
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-10-21 18:39:09 +0800

    oops

--- resource/search.js
+++ resource/search.js
@@ -1,4 +1,4 @@
-var searchInput = document.getElementById("searchbox");
+var searchInput = document.getElementById("searchbox").value;
 
 const searchGoogle = document.getElementById("google");
 
@@ -7,16 +7,16 @@ const searchBing = document.getElementById("bing");
 const searchWP = document.getElementById("wikipedia");
 
 searchGoogle.onclick = function() {
-    var searchInput = document.getElementById("searchbox").value;
-  window.location.href("https://google.com/search?q=" + searchInput);
+  var searchInput = document.getElementById("searchbox").value;
+  window.location.href = "https://google.com/search?q=" + searchInput;
 };
 
 searchBing.onclick = function() {
-    var searchInput = document.getElementById("searchbox").value;
-  window.location.href("https://bing.com/search?q=" + searchInput);
+  var searchInput = document.getElementById("searchbox").value;
+  window.location.href = "https://bing.com/search?q=" + searchInput;
 };
 
 searchWP.onclick = function() {
-    var searchInput = document.getElementById("searchbox").value;
-  window.location.href("https://en.wikipedia.org/w/index.php?title=Special:Search&search=" + searchInput);
+  var searchInput = document.getElementById("searchbox").value;
+  window.location.href = "https://en.wikipedia.org/w/index.php?title=Special:Search&search=" + searchInput;
 }
