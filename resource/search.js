43ef88bdfa293d01751a18a4f69f8ba7dd1b21ce
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-10-25 18:39:03 +0800

    placeholder

--- resource/search.js
+++ resource/search.js
@@ -1,3 +1,5 @@
+// Search function
+
 var searchInput = document.getElementById("searchbox").value;
 
 const searchGoogle = document.getElementById("google");
@@ -6,17 +8,30 @@ const searchBing = document.getElementById("bing");
 
 const searchWP = document.getElementById("wikipedia");
 
-searchGoogle.onclick = function() {
+searchGoogle.onclick = function () {
   var searchInput = document.getElementById("searchbox").value;
   window.location.href = "https://google.com/search?q=" + searchInput;
 };
 
-searchBing.onclick = function() {
+searchBing.onclick = function () {
   var searchInput = document.getElementById("searchbox").value;
   window.location.href = "https://bing.com/search?q=" + searchInput;
 };
 
-searchWP.onclick = function() {
+searchWP.onclick = function () {
   var searchInput = document.getElementById("searchbox").value;
   window.location.href = "https://en.wikipedia.org/w/index.php?title=Special:Search&search=" + searchInput;
 }
+
+// API for weather
+fetch('https://api.weather.gov', {
+  method: 'GET'
+})
+  .then(res => {
+    if (res.ok) {
+      console.log("SUCCESS LOADING WEATHER API")
+    }
+  },
+    res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
