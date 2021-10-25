7f39a80f07a8234826864c2e5b11a0cb23525285
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-10-25 17:58:24 +0800

    fix everything

--- resource/search.js
+++ resource/search.js
@@ -8,15 +8,15 @@ const searchWP = document.getElementById("wikipedia");
 
 searchGoogle.onclick = function() {
   var searchInput = document.getElementById("searchbox").value;
-  window.location.href = "https://google.com/search?q=" + searchInput;
+  window.location.replace("https://google.com/search?q=" + searchInput);
 };
 
 searchBing.onclick = function() {
   var searchInput = document.getElementById("searchbox").value;
-  window.location.href = "https://bing.com/search?q=" + searchInput;
+  window.location.replace("https://bing.com/search?q=" + searchInput);
 };
 
 searchWP.onclick = function() {
   var searchInput = document.getElementById("searchbox").value;
-  window.location.href = "https://en.wikipedia.org/w/index.php?title=Special:Search&search=" + searchInput;
+  window.location.replace("https://en.wikipedia.org/w/index.php?title=Special:Search&search=" + searchInput)
 }
