e60efa08ad4a66060c6066704e970427e0d63168
Markus <65797058+headquarter8302@users.noreply.github.com>
2022-05-26 17:51:18 +0800

    Update script.js

--- resource/script.js
+++ resource/script.js
@@ -3,17 +3,15 @@ document.__defineGetter__("cookie", function() { return '';} );
 document.__defineSetter__("cookie", function() {} );
 
 function changeTheme() {
-    let isChanged = false;
-    if (isChanged) {
-        document.getElementById('html').style.filter = "initial";
-        isChanged = false;
-    } else {
-        document.getElementById('html').style.filter = "invert\(1\) hue-rotate\(180deg\)";
-        isChanged = true;
-    }
+  let isChanged = false;
+  if (isChanged) {
+    document.getElementById('html').style.filter = "initial";
+    isChanged = false;
+  }
+    document.getElementById('html').style.filter = "invert\(1\) hue-rotate\(180deg\)";
+    isChanged = true;
 }
 
 console.log(
-        "%cWhatcha doin here?",
-        "color:cyan;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif;font-size:3rem;font-weight:bold"
-      );
+  "%cWhatcha doin here?",
+  "color:cyan;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif;font-size:3rem;font-weight:bold");
