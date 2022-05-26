6f6cdfbe9138def65ffb71615b278e5c141b2afc
Markus <65797058+headquarter8302@users.noreply.github.com>
2022-05-26 18:01:17 +0800

    Update script.js

--- resource/script.js
+++ resource/script.js
@@ -8,11 +8,11 @@ function changeTheme() {
     document.getElementById('html').style.filter = "initial";
     isChanged = false;
   } else {
-    document.getElementById('html').style.filter = "invert\(1\) hue-rotate\(180deg\)";
+    document.getElementById('html').style.filter = "invert\(1\)";
     isChanged = true;
   }
 }
 
 console.log(
-  "%cWhatcha doin here?",
+  "%cWhatchoo doin here?",
   "color:cyan;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif;font-size:3rem;font-weight:bold");
