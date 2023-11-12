1c03f91d3b6cd4bf88dbb4520e75189ba391c717
Markus <65797058+headquarter8302@users.noreply.github.com>
2023-11-11 23:47:44 -0400

    Update script.js

--- resource/script.js
+++ resource/script.js
@@ -1,6 +1,14 @@
 const cursor = document.getElementById("cursor");
 const links = document.querySelectorAll("a");
 
+document.addEventListener("mousedown", () => {
+  cursor.classList.add("clicked");
+});
+
+document.addEventListener("mouseup", () => {
+  cursor.classList.remove("clicked");
+});
+
 links.forEach(function (e) {
   e.addEventListener("mouseover", function () {
     cursor.classList.add("link-hover");
@@ -13,13 +21,18 @@ links.forEach(function (e) {
 
 document.addEventListener("mousemove", function (e) {
   if (cursor.classList.contains("link-hover")) {
-    cursor.style.transform = `translate(${e.clientX - 25}px, ${e.clientY - 25}px)`;
+    cursor.style.transform = `translate(${e.clientX - 25}px, ${
+      e.clientY - 25
+    }px)`;
   } else {
-    cursor.style.transform = `translate(${e.clientX - 10}px, ${e.clientY - 10}px)`;
+    cursor.style.transform = `translate(${e.clientX - 10}px, ${
+      e.clientY - 10
+    }px)`;
   }
   cursor.style.display = "block";
 });
 
 console.log(
-  "%cWhatchoo doin here?",
-  "color:cyan;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif;font-size:3rem;font-weight:bold");
+  "%cHey.",
+  'font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;font-size:3rem;font-weight:bold'
+);
