69942495fd88590bebe512ad4cec4f6f365fec99
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-03-11 22:17:45 -0300

    install sitemap deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 export default defineConfig({
-  integrations: [],
+  integrations: [sitemap()],
   site: 'https://headquarter8302.github.io',
   scopedStyleStrategy: 'class',
   compressHTML: false,
