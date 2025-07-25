34b8ea9c3fe1113d14d2fe6d3ffe9e8fe15b41b9
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-07-25 00:20:59 -0300

    prepare for preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import preact from '@astrojs/preact';
 export default defineConfig({
   integrations: [
     sitemap({
       filter: (page) =>
         !page.includes(".old") &&
         !page.includes("dumpster"),
-    })],
+    }),
+    preact()
+  ],
   site: 'https://headquarter8302.github.io',
   // scopedStyleStrategy: 'class',
   devToolbar: {
